9223c6af1e990125d558a2b17325ce585cbdadc4
OS_GPCP_GPDN42\admin <dongvq@tasc.vn>
2022-08-01 15:23:50 +0700

    Initial commit

---
+++ src/main/java/com/example/webdoctruyen/WebDocTruyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdoctruyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDocTruyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDocTruyenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webdoctruyen/WebDocTruyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdoctruyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDocTruyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
